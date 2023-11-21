7a8b33ec866a68d6826b35472f8244a59df63016
Chhen <hennie242@gmail.com>
2023-11-21 09:35:46 +0100

    TjekPrimtal

---
+++ W47Teknologi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace W47Teknologi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(" Indtast et tal for at tjekke om det er et primtal ");
+
+                uint input = Convert.ToUInt32(Console.ReadLine());
+
+                if (IsPrime(input))
+                {
+                    Console.WriteLine(IsPrime(input));
+                }
+                else
+                    Console.WriteLine(IsPrime(input));
+            }
+        }
+
+        public static bool IsPrime(uint number)
+        {
+            if (number == 0) return false;
+
+            for (uint i = 2; i < number; i++)
+                if (number % i == 0)
+                    return false;
+            return true;
+
+        }
+    }
+}
